9b8d0ca7c2b0f9fe89f5a1fa9364600c0840d9e0
yangrui <yangrui@yangruideMac-mini.local>
2018-12-18 13:30:32 +0800

    s

---
+++ TextReg/国际化比对工具/RGLocalization.h
@@ -0,0 +1,29 @@
+//
+//  RGLocalization.h
+//  TextReg
+//
+//  Created by yangrui on 2018/12/18.
+//  Copyright © 2018 yangrui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RGLocalization : NSObject
+/**
+ 检查前面的键有没有重复的, 重复的将被打印出来
+ "debug"="debug";
+ "isChina"="isChina";
+ "Setup failed"="Setup failed";
+ "Next"="Next";
+ "Hubs"="Hubs";
+ "Search"="Add";
+ "debug"="debug";
+ "isChina"="isChina";
+ "Setup failed"="Setup failed";
+ "Next"="Next";
+ "Hubs"="Hubs";
+ "Search"="Add";
+ */
++(void)compareRepeatPath:(NSString *)path;
+@end
+
---
+++ TextReg/国际化比对工具/RGLocalization.m
@@ -0,0 +1,59 @@
+//
+//  RGLocalization.m
+//  TextReg
+//
+//  Created by yangrui on 2018/12/18.
+//  Copyright © 2018 yangrui. All rights reserved.
+//
+
+#import "RGLocalization.h"
+
+#import "NSString+reg.h"
+
+@implementation RGLocalization
+
+/**
+ 检查前面的键有没有重复的, 重复的将被打印出来
+ "debug"="debug";
+ "isChina"="isChina";
+ "Setup failed"="Setup failed";
+ "Next"="Next";
+ "Hubs"="Hubs";
+ "Search"="Add";
+ "debug"="debug";
+ "isChina"="isChina";
+ "Setup failed"="Setup failed";
+ "Next"="Next";
+ "Hubs"="Hubs";
+ "Search"="Add";
+ */
++(void)compareRepeatPath:(NSString *)path{
+    NSString *fileStr = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    
+    // 将 本地语言一句句的 正则取出来
+    NSString *pattern = @"[\"]{1}.*[\"]{1}[=]{1}[\"]{1}.*[\"]{1};?";
+    NSArray *arr = [fileStr subStringArrWithPattern:pattern];
+    NSMutableArray *newArrM = [NSMutableArray array];
+    for(int i = 0; i < arr.count; i++){
+        NSString *prefixStr = [[arr[i] componentsSeparatedByString:@"="] firstObject];
+        [newArrM addObject:prefixStr];
+    }
+    
+    NSMutableArray *repeatArrM = [NSMutableArray array];
+    for (int a = 0 ; a< newArrM.count ; a++){
+        NSString *temA = newArrM[a];
+        if (a < newArrM.count - 1) {
+            for (int b = a+1; b < newArrM.count; b++) {
+                NSString *tempB = newArrM[b];
+                if ([temA isEqualToString:tempB]) {
+                    // 将重复的key  打印出来
+                    NSLog(@"重复===> %@",temA);
+                    break;
+                }
+            }
+        }
+        
+    }
+}
+    
+@end
